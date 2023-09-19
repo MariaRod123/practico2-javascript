8f5464a6d6dd1eb3cbe1ccfa7613f4559fc6dd5b
María Rodríguez <marodriguezpiriz08@gmail.com>
2023-09-19 19:18:11 -0300

    agrego tercera parte ejercicio1

---
+++ ejercicio1-parteC.js
@@ -0,0 +1,22 @@
+const array_inicial = ["B", "B", 1, 1, 1, 2, 2, 0];
+const numeros_agrupados = [];
+
+
+for (const elemento of array_inicial) {
+  if (typeof elemento === 'number') {
+    if (elemento in numeros_agrupados) {
+      numeros_agrupados[elemento]++;
+    } else {
+      numeros_agrupados[elemento] = 1;
+    }
+  }
+}
+
+const array_salida= [];
+for (const numero in numeros_agrupados) {
+  if (numeros_agrupados[numero] > 2 || (numeros_agrupados[numero] >= 1 && parseInt(numero) === 0)) {
+    array_salida.unshift(parseInt(numero));
+  }
+}
+
+console.log(array_salida); 
