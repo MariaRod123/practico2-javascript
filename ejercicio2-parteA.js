bc86ed8debc8f99f72e4b5ea0f672bdb6adf439b
María A. Rodríguez <72228855+MariaRod123@users.noreply.github.com>
2023-09-25 11:48:16 -0300

    subido ejercicio 2 parte a

---
+++ ejercicio2-parteA.js
@@ -0,0 +1,35 @@
+const array_entrada = [1, 2, "B", "C", "B"];
+
+function clasificarElementos(array_inicial) {
+  const numeros = [];
+  const letras = [];
+  //const nulos = [];
+ 
+
+  for (const elemento of array_inicial) {
+    if (typeof elemento === 'number') {
+      numeros.push(elemento);
+    } else if (typeof elemento === 'string') {
+      letras.push(elemento);
+    } 
+  }
+
+  return { numeros, letras};
+}
+
+const clasificacion = clasificarElementos(array_entrada);
+
+
+console.log('Números:', clasificacion.numeros); 
+const array_letras_repetidas = clasificacion.letras.sort();
+console.log('Letras:', array_letras_repetidas); 
+
+const array_compactado = [];
+
+for (const letra of array_letras_repetidas) {
+  if (!array_compactado.includes(letra)) {
+    array_compactado.push(letra);
+  }
+}
+
+console.log('Array de letras sin repetir:', array_compactado); 
